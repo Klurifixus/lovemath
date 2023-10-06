644b3fcf958bd83c6c31d60e7952210d36c812fd
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-06 17:24:10 +0200

    done js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,49 @@
-function funGame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
-function checkAnswer(){
-
-}
-function calculateCorrectAnswear(){
-
-}
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionalQuestion(){
+function incrementScore() {
 
 }
 
-function displaySubstactQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
     
 }
-
