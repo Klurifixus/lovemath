1d970e1de9bc130ff72e01390e1f8b01ed18fe6a
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-06 20:43:43 +0200

    did shit, saw shit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -33,6 +33,17 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculateCorrectAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Thats RIGHT!");
+    }   else{
+        alert(`Naaaaw, thats WRONG with ${userAnswer}. ${calculatedAnswer} is the one![0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
 
 }
 
