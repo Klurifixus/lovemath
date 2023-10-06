932822e84e4c23fc8b6c09a5b51519b69e60ea1f
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-06 16:27:52 +0200

    add js script

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function funGame() {
+
+}
+
+function checkAnswer(){
+
+}
+function calculateCorrectAnswear(){
+
+}
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionalQuestion(){
+
+}
+
+function displaySubstactQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
